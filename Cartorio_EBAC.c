1e20e9fc339d0a0ee00a3d7dbd78799ef4304704
FlippyFly <155852571+FlippyFly@users.noreply.github.com>
2024-01-06 18:55:57 -0300

    Opção de sair
    incluso no sistema a opçao de saida para o usuario

--- Cartorio_EBAC.c
+++ Cartorio_EBAC.c
@@ -130,6 +130,7 @@ int main()
 	    printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar nomes\n");
 	    printf("\t3 - Deletar nomes\n\n"); 
+	    printf("\t4 - sair di sistema\n\n");
 	    printf("Opção: "); //fim do menu
 	
 	    scanf("%d",&opcao);  //armazenando a escolha do usuario
@@ -149,6 +150,11 @@ int main()
 			case 3:
 		    deletar();
 		    break;
+		    
+		    case 4:
+		    printf("Obrigado por utilizar o sistema\n\n");
+			return 0;
+			break;	
 		    
 		     default:
 			printf("Essa opção não está disponivel!\n");
